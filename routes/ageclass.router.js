dd110f8d03a0233f1233973bf688cee44d1aa4eb
Paul Fournier <paul.fournier.3314@gmail.com>
2022-12-13 16:37:32 +0100

    add actor, ageclass, category, producer.router.js files

---
+++ routes/ageclass.router.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const ageclassController = require('../controllers/ageclass.controller');
+
+require('dotenv').config();
+
+const router = express.Router();
+
+router.post("/create", ageclassController.createAgeClass);
+router.delete("/delete", ageclassController.deleteAgeclass);
+router.put("/update", ageclassController.updateAgeclass);
+router.get("/", ageclassController.findAllAgeClasss);
+
+// export default router;
+module.exports = router;
---
+++ routes/category.router.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const categoryController = require('../controllers/category.controller');
+
+require('dotenv').config();
+
+const router = express.Router();
+
+router.post("/create", categoryController.createCategory);
+router.delete("/delete", categoryController.deleteCategory);
+router.put("/update", categoryController.updateCategory);
+router.get("/", categoryController.findAllCategorys);
+
+// export default router;
+module.exports = router;
