e4c55390ade7677e19afe52ef6c39b548268cb96
kenb9027 <kenb9027@gmail.com>
2022-12-13 09:47:12 +0100

    fix migrations

--- migrations/20221212132313-create-movie.js
+++ migrations/20221212132313-create-movie.js
@@ -18,18 +18,6 @@ module.exports = {
       resume: {
         type: Sequelize.STRING
       },
-      director_id: {
-        type: Sequelize.INTEGER
-      },
-      producer_id: {
-        type: Sequelize.INTEGER
-      },
-      category_id: {
-        type: Sequelize.INTEGER
-      },
-      ageclass_id: {
-        type: Sequelize.INTEGER
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
